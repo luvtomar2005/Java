2f4136c6484dcc40b1772a1ea658793533da912d
luvtomar2005 <luvtomar2005@gmail.com>
2026-06-01 11:19:18 +0530

    Packages in java
    Understood the concept of packages in java , how to create them , and how to use them ,  importance of keeping the classes as public

---
+++ program_10_package.java
@@ -0,0 +1,17 @@
+/* Undestanding packages in java */
+package Package1;
+
+public class program_10_package {
+    private int x;
+    private int y;
+
+    public void setData(int p, int q) {
+        x = p; 
+        y = q;
+    }
+
+    public void Display() {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
